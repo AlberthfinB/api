917a3bbc576a1aa55e967334e59801131011f655
novan_manday <52309597+novanmasriandi11@users.noreply.github.com>
2024-11-17 20:09:42 +0700

    Feat: Add table referral
    Feat: Add table referral

--- prisma/migrations/20241116115617_update_table_transaction_and_transaction_detail/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `transaction` ADD COLUMN `coupon_id` INTEGER NULL;
-
--- AlterTable
-ALTER TABLE `transaction_detail` ADD COLUMN `discount` DOUBLE NOT NULL DEFAULT 0.0;
-
--- AddForeignKey
-ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_coupon_id_fkey` FOREIGN KEY (`coupon_id`) REFERENCES `Coupon`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20241117085316_added_field_referee_role_in_table_referral/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `referral` ADD COLUMN `referee_role` VARCHAR(191) NOT NULL DEFAULT 'attendee';
