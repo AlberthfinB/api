86d7a956c870fc709807bead7aa09d6ef800f488
AlberthfinB <alberthfinb@gmail.com>
2024-11-18 18:08:28 +0700

    cors hit client

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,23 @@
 import express, { Application } from "express";
-import { PORT as port } from "./utils/envConfig";
+import {BASE_WEB_URL, PORT as port } from "./utils/envConfig";
 import ErrorMiddleware from "./middlewares/error.middleware";
 import authRouter from "./routes/auth.route";
 import firstSeedingData from "./utils/firstSeedingData";
 import eventRouter from "./routes/event.route";
+import cors from "cors";
 
 const PORT = Number(port) || 7000;
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(
+    cors({
+       origin: BASE_WEB_URL || "http://localhost:3000",
+       credentials: true,
+    })
+ );
 
 app.use("/management", eventRouter);
 app.use('/auth', authRouter);
