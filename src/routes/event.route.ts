c4e819a802fa8a17f105fe7d80a83151e6fdc074
AlberthfinB <alberthfinb@gmail.com>
2024-11-18 14:19:57 +0700

    tambah oraganizer guard

--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createEvent ,getEventsIncoming } from "../controllers/event.controller";
-import { verifyToken } from "../middlewares/auth.middleware";
+import { verifyToken,EventOrganizerGuard } from "../middlewares/auth.middleware";
 import { SingleUploader } from "../utils/uploaderCloudinary";
 
 const eventRouter = Router();
 
 const { uploadMiddleware } = SingleUploader("event_image", "events"); 
 
-eventRouter.post("/create-event",verifyToken,uploadMiddleware, createEvent);
+eventRouter.post("/create-event",verifyToken,EventOrganizerGuard, uploadMiddleware, createEvent);
 
 eventRouter.get("/events",verifyToken, getEventsIncoming );
 
